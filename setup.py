00200a01f5259a8c27272b50c435c3caba7cdfae
a.amiri <a.amiri@kian.digital>
2020-12-31 00:43:01 +0330

    removed build folders

--- setup.py
+++ setup.py
@@ -3,14 +3,12 @@ import setuptools
 
 from pip._internal.req import parse_requirements
 
-reqs = [str(ir.req) for ir in parse_requirements('requirements.txt', session='hack')]
 
 setup(
     name='iploader',
-    version='RC-0.1',
+    version='1.0',
     packages=setuptools.find_packages(),
     long_description=open('README.md').read(),
     scripts= ['bin/iploader',],
-    install_requires= reqs
 )
 
